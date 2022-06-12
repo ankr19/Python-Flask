e81fad98c92aea05220d017ad48946ca802c08a3
ankr19 <ankm335@gmail.com>
2022-06-12 18:03:37 +0530

    2nd ecom app
    Structure of ecom app

---
+++ E-commerce/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

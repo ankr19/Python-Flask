000d695089eb8aa3dad9a2b938ad0997f8468b45
ankr19 <ankm335@gmail.com>
2022-06-22 20:30:10 +0530

    changing
    changing things

--- E-commerce/app.py
+++ E-commerce/app.py
@@ -1,13 +1,14 @@
+from crypt import methods
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/product')
-def product():
+@app.route('/product/<string:name>/', methods=['GET','POST'])
+def product(name):
+    
     return render_template('product.html')
 
 if __name__ == "__main__":
